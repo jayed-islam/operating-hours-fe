a36150c1d48b5cffcaf72dd8bf87f8548f8c0aa6
jayed-phero <jayed.programminghero@gmail.com>
2024-09-23 16:21:44 +0600

    update to main view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import OperatingHoursView from "@/sections/operating-hours/view/operating-hours-view";
 
+export const metadata = {
+  title: "Operating hours",
+};
+
 export default function Home() {
   return <OperatingHoursView />;
 }
